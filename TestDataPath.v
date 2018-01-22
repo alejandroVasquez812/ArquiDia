737b37eb1336182bc6a66aa96c4e78d886b4407b
jrsm1 <johnnyrsm1@gmail.com>
2018-01-21 21:32:18 -0400

    Spreadsheet Correction

--- TestDataPath.v
+++ TestDataPath.v
@@ -3,7 +3,7 @@ module TestDataPath;
 
 //Inputs
 reg[7:0] Data;
-reg [5:0] OpXX, Clear_Select;
+reg [5:0] OpXX;
 reg[1:0]type, MA, MB, MNP, MP, MS, MSc;
 reg IR_Ld, MAR_Ld, MDR_Ld, WIM_Ld, TBR_Ld, TTR_Ld, PC_Ld, NPC_Ld, nPC_Clr, PSR_Ld, FR_Ld, RW, MOV, MC, MF, MM, MOP, MSa, 
 	Register_Windows_Enable, RF_Load_Enable, RF_Clear_Enable, Clk;
@@ -39,40 +39,7 @@ initial begin
 
 
 end
-/*
-initial fork
-	//Store State 25
-	Register_Windows_Enable = 1'b1;
-	RF_Load_Enable = 1'b1;
-	RF_Clear_Enable = 1'b0;
-	OpXX = 5'b00000;
-	IR_Ld = 1'b0;
-	MAR_Ld = 1'b1;
-	MDR_Ld = 1'b0;
-	WIM_Ld = 1'b0;
-	TBR_Ld = 1'b0;
-	TTR_Ld = 1'b0;
-	PC_Ld = 1'b0;
-	NPC_Ld = 1'b0;
-	nPC_Clr = 1'b0;
-	PSR_Ld = 1'b0;
-	RW = 1'b1;
-	MOV = 1'b0;
-	type = 1'b0;
-	FR_Ld = 1'b0;
-	MA = 1'b0;
-	MB = 1'b0;
-	MC = 1'b0;
-	MF = 1'b0;
-	MM = 1'b1;
-	MNP = 1'b0;
-	MOP = 1'b1;
-	MP = 1'b0;
-	MSa = 1'b0;
-	MSc = 1'b0;
 
-join
-*/
 initial fork
 	//Reset State 0
 	Register_Windows_Enable = 1'b1;
@@ -91,18 +58,36 @@ initial fork
 	PSR_Ld = 1'b0;
 	RW = 1'b1;
 	MOV = 1'b0;
-	type = 1'b0;
+	type = 2'b00;
 	FR_Ld = 1'b0;
-	MA = 1'b0;
-	MB = 1'b0;
+	MA = 2'b00;
+	MB = 2'b00;
 	MC = 1'b0;
 	MF = 1'b0;
 	MM = 1'b0;
-	MNP = 1'b0;
+        MP = 2'b00;
+	MNP = 2'b00;
 	MOP = 1'b0;
 	MP = 1'b0;
+	MS = 2'b00;
 	MSa = 1'b0;
-	MSc = 1'b0;
+	MSc = 2'b00;
+
+	#2
+	//Fetch1
+
+
+//Fetch
+
+
+//Fetch 3
+
+
+//Decode
+
+
+//State 11
+
 join
 
 
